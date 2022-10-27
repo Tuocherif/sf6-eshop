12fa778e50bcc81785f2fcd185e55c7bce3383b5
FranckDodo91 <devlogappdgmf@gmail.com>
2022-10-27 15:25:46 +0200

    Creation of one hundred fictitious products and loading of these products into the Database

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,21 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Création de 100 produits
+        for ($p=0; $p < 100; $p++) { 
+            $product = new Product();
+            $product
+                ->setName("Produit No : $p")
+                ->setPrice(mt_rand(100, 200))
+                ->setSlug("produit-$p")
+            ;
 
+            // Préparation de la migration vers la Base de données
+            $manager->persist($product);
+        }
+
+        // Envoie des 100 produits vers la Base de données
         $manager->flush();
+
     }
 }
