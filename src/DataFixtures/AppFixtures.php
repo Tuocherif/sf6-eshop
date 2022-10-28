b38e0748b38e710ccd851cecd278c52c01dcdb21
FranckDodo91 <devlogappdgmf@gmail.com>
2022-10-28 06:52:21 +0200

    Creation of one hundred fictitious products with the 'FakerPHP/Faker' library and loading of these fictitious products in the Database

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,21 +2,29 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use Faker\Provider;
 use App\Entity\Product;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        // Création du instance de 'Faker\Generator'
+        $faker = Factory::create('fr_FR');
+
         // Création de 100 produits
         for ($p=0; $p < 100; $p++) { 
             $product = new Product();
             $product
-                ->setName("Produit No : $p")
+                /* ->setName("Produit No : $p")
                 ->setPrice(mt_rand(100, 200))
-                ->setSlug("produit-$p")
+                ->setSlug("produit-$p") */
+                ->setName($faker->sentence())
+                ->setPrice($faker->randomNumber(3))
+                ->setSlug($faker->slug())
             ;
 
             // Préparation de la migration vers la Base de données
