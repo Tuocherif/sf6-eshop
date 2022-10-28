53da3cea1d217c45d20f45ea480c4bf329adc995
FranckDodo91 <devlogappdgmf@gmail.com>
2022-10-28 07:35:59 +0200

    Creation of more realistic fictitious product names with the 'mbezhanov/faker-provider-collection' library and loading in the DB

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,12 @@
 
 namespace App\DataFixtures;
 
+use Bezhanov;
 use Faker\Factory;
 use Faker\Provider;
 use App\Entity\Product;
 use Liior\Faker\Prices;
+use Bezhanov\Faker\Provider\Commerce;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -17,6 +19,8 @@ class AppFixtures extends Fixture
         $faker = Factory::create('fr_FR');
         // Ajout de la librairie 'liorchamla/faker-prices'
         $faker->addProvider(new Prices($faker));
+        // Ajout de la librairie 'mbezhanov/faker-provider-collection'
+        $faker->addProvider(new Commerce($faker));
 
         // symfony console 
 
@@ -27,7 +31,8 @@ class AppFixtures extends Fixture
                 /* ->setName("Produit No : $p")
                 ->setPrice(mt_rand(100, 200))
                 ->setSlug("produit-$p") */
-                ->setName($faker->sentence())
+                /* ->setName($faker->sentence()) */
+                ->setName($faker->productName)
                 ->setPrice($faker->price(4000, 20000))
                 ->setSlug($faker->slug())
             ;
